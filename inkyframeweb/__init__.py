6f352f69795bf6f8a5057cd6ae37029ec0cfd320
Louis King <jinglemansweep@gmail.com>
2024-06-03 17:11:45 +0100

    Initial commit

---
+++ inkyframeweb/__init__.py
@@ -0,0 +1,6 @@
+from .utils import read_version_from_pyproject
+
+_APP_AUTHOR = "JingleManSweep"
+_APP_NAME = "inkyframeweb"
+_APP_TITLE = "Inky Frame Web"
+_APP_VERSION = read_version_from_pyproject()
